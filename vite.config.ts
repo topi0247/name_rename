49fb7915461f2cac5662c44ae719d0b1c988b274
topi0247 <karubona.ra247@gmail.com>
2025-02-09 17:16:19 +0900

    feat: shadcn/ui導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 8000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
